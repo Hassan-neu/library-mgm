83a10211e729b82cd7b47168a9860af7b3fb8128
Hassan Adeyemi <hassanadeyemi01@gmail.com>
2023-07-13 23:11:52 +0100

    :package: changed tooltip background color

--- src/components/tooltip.jsx
+++ src/components/tooltip.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const ToolTip = ({ children }) => {
     return (
-        <div className="hidden absolute top-full translate-y-3 left-1/2 -translate-x-1/2 bg-gray-300 text-white text-[10px] w-20 rounded-md tooltip">
+        <div className="hidden absolute top-full translate-y-2 left-1/2 -translate-x-1/2 bg-slate-800 text-white text-[10px] w-20 rounded-md tooltip">
             <div className="relative px-1 py-1">
                 <span className="w-full block">{children}</span>
-                <span className="block w-3 h-3 -z-20 bg-gray-300 rotate-45 absolute left-1/2 top-0 -translate-y-1/2 -translate-x-1/2"></span>
+                <span className="block w-2 h-2 -z-20 bg-slate-800 rotate-45 absolute left-1/2 top-0 -translate-y-1/2 -translate-x-1/2"></span>
             </div>
         </div>
     );
